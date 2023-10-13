58d22d135f3df3414272d79b709f96fe87acdaba
Andrew Desmarais <andydes@amazon.com>
2023-10-13 10:34:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://main.dhzz9tafaw6fx.amplifyapp.com/',
+  assetPrefix: 'https://main.dhzz9tafaw6fx.amplifyapp.com',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'main.d45dn2ti2s07n.amplifyapp.com'],
